5f1339284b8e2eb1a2b27cab9da5f36013f5b319
Mel Phillips <melphillips@Mels-MacBook-Pro-2.local>
2022-07-19 17:01:45 -0400

    adding data to Flask application.

---
+++ OLD_app.py
@@ -0,0 +1,11 @@
+# Import the Flask Dependency
+from flask import Flask
+
+# Create a new Flask App Instance
+app = Flask(__name__)
+
+# Create Flask Routes
+@app.route('/') # forward slash indicates we're putting our data at the root of our route
+
+def hello_world():
+    return 'Hello world'
